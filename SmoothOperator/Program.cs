fee7d171a2046bd7c31f5c5eece6946afa5a3bd6
Batmetista <batista.as.pedro@gmail.com>
2024-03-01 00:05:43 +0000

    EX1

---
+++ SmoothOperator/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SmoothOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insira um número inteiro não-negativo.");
+            string input = Console.ReadLine();
+            int number = int.Parse( input );
+            if (number >= 0 && number <= byte.MaxValue)
+            {
+                byte n = (byte)number;
+
+                Console.WriteLine($"Resultado da operação de decremento de {n}: {--n}");
+                Console.WriteLine($"Resultado da operação de incremento de {n}: {++n}");
+            }
+            else
+            {
+                Console.WriteLine("Número inválido. Certifique-se de digitar um número inteiro não-negativo dentro do intervalo permitido para um byte (0 a 255).");
+            }
+        }
+    }
+}
