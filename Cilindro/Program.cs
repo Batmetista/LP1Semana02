dc3452c70f3ff30ce448a0d23d9be885b1e4d410
Batmetista <batista.as.pedro@gmail.com>
2024-03-01 16:19:23 +0000

    EX5

---
+++ Cilindro/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Cilindro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite a altura do cilindro:");
+            float altura = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o raio do cilindro:");
+            float raio = float.Parse(Console.ReadLine());
+
+            float volume = MathF.PI * MathF.Pow(raio, 2) * altura;
+            float areaSuperficie = 2 * MathF.PI * raio * (raio + altura);
+
+            Console.WriteLine($"Volume do cilindro: {volume}");
+            Console.WriteLine($"Área de superfície do cilindro: {areaSuperficie}");
+        }
+    }
+}
