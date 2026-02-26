e188e3e0701da7747af53897085c82820a6cf87e
Pedro Batista <batista.as.pedro@gmail.com>
2026-02-26 16:14:54 +0000

    add variables int and sting

--- TempConvert/Program.cs
+++ TempConvert/Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Insira o vlaor da temperatura: ");
+int t = Console.ReadLine();
+Console.WriteLine("Insira a unidade de origem: ");
+string u = Console.ReadLine();
