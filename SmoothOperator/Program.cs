305671f05c3aac660fd83fd907b26fe46db8406e
Pedro Batista <batista.as.pedro@gmail.com>
2026-02-26 16:05:20 +0000

    EX1

---
+++ SmoothOperator/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MeuPrograma
+{
+    /// <summary>
+    /// Classe principal
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// Método principal
+        /// </summary>
+        /// <param name="args"></param>
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Insere número inteiro: ");
+            sbyte n = Console.ReadLine();
+            sbyte m = --n;
+            sbyte l = ++m;
+            console.WriteLine(m);
+            console.WriteLine(l);
+        }
+    }
+}
